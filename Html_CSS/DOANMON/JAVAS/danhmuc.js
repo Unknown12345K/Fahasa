06c9c3a56f15dd39aa0bb7d6124389bec8c1ac5e
Your Name <ngogiatun873@gmail.com>
2024-06-01 21:17:09 +0700

    update

--- Html_CSS/DOANMON/JAVAS/danhmuc.js
+++ Html_CSS/DOANMON/JAVAS/danhmuc.js
@@ -5,3 +5,19 @@
         menu.classList.toggle("block")
     })
  })
+// cart-modal------>
+ const openmodalclick = document.querySelector(".sub-cart")
+ const iconclose = document.querySelector(".cart-modal-head .cart-close")
+ const modal = document.querySelector(".cart-modal")
+
+function togglemodal(a) {
+    modal.classList.toggle("hidden")
+}
+
+ openmodalclick.addEventListener("click",togglemodal)
+ iconclose.addEventListener("click",togglemodal)
+ modal.addEventListener("click",function (a) {
+        if (a.target === a.currentTarget) {
+            togglemodal()
+        }
+ })
